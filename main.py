7bb373d187e2aabcce682c02918d5a9e88461179
20reza <161266780+20reza@users.noreply.github.com>
2024-05-14 00:55:45 +0000

    starting window

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
-from tkinter import * 
+from tkinter import *
+from PIL import Image, ImageTk
 
 
 class QuizStarter:
     def __init__(self, parent):
-        background_color="OldLace"
+
+        #background_color = "OldLace"
+        background_color = "red"
+
+
+        self.bg_image = Image.open("QuizQuestion.png") #need to use Image if need to resize
+        #self.bg_image = self.bg_image.resize((450, 350), Image.LANCZOS)  # Resize the image with specified dimensions and using Image.LANCZOS
+
+        width = 960
+        height = 540
+        self.resize_image = self.bg_image.resize((width, height))
+
+        self.bg_image = ImageTk.PhotoImage(self.resize_image)
+
+
         #frame set up
-        self.quiz_frame = Frame(parent, bg=background_color, padx=100, pady=100)
-        self.quiz_frame.grid()
+        self.quiz_frame = Frame(parent, bg=background_color)
+        self.quiz_frame.pack()
 
-        #label widget for our heading
-        self.heading_label = Label(
-            self.quiz_frame,
-            text="General Quiz",
-            font=("Tw cen MT", 18, "bold")
-        )
-        self.heading_label.grid(row=0)                             
+        #label for image
+        self.image_label= Label(self.quiz_frame, image=self.bg_image)
+        #self.image_label.place(x=0, y=0, relwidth=1, relheight=1) # make label l to fit the parent window always
+        self.image_label.pack()
 
 
 
-#**********************starting point of program**********************#
+
+
+#***********Starting point of the program***********#
 if __name__ == '__main__':
     root = Tk()
-    root.title("General quiz")
-    quizStarter_object = QuizStarter(root) #instantiation, making an intance of class quizstarter to createthe frame with its widgets, passing roots as a parameter
-    root.mainloop()#so the windows doesn't dissapear
+    root.geometry("960x540")
+    root.title("General Knowledge Quiz")
+    quizstarter_object = QuizStarter(root)  
+    root.mainloop() #So window won't disappear 
